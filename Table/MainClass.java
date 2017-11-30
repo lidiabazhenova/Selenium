64e85b6e4def4eabee8b094e14837ff1c4e6e5f2
Lidia <lidiabazhenova@gmail.com>
2017-11-30 13:52:14 +0300

    table

---
+++ Table/MainClass.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainClass {
+
+    static WebDriver driver;
+
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\student\\IdeaProjects\\testselenium\\drivers\\geckodriver.exe");
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("http://www.w3schools.com/html/html_tables.asp");
+
+        WebElement tableElement = driver.findElement(By.xpath("//table[@id=\"customers\"]"));
+
+        Table table = new Table(tableElement, driver);
+
+        System.out.println("Rows number is: " + table.getRows().size());
+
+        System.out.println(table.getValueFromCell(2, 3));
+        System.out.println(table.getValueFromCell(4, 1));
+
+        System.out.println(table.getValueFromCell(4, "Company"));
+        System.out.println(table.getValueFromCell(1, "Country"));
+        System.out.println(table.getValueFromCell(2, "Contact"));
+
+
+        driver.quit();
+
+    }
+}
