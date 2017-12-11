d761c5c86d42f2e4c0e16e7358f8079f6f57c447
Lidia <lidiabazhenova@gmail.com>
2017-12-11 13:22:32 +0300

    PageObject

--- PageObjectSeleniumTest/src/main/java/com/lidiabazhenova/MainClass.java
+++ PageObjectSeleniumTest/src/main/java/com/lidiabazhenova/MainClass.java
@@ -1,8 +1,23 @@
 package com.lidiabazhenova;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainClass {
+    private static WebDriver driver;
 
     public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "C:\\projects\\Selenium\\PageObjectSeleniumTest\\drivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://github.com/");
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.registration("testusername00999", "ggg@mail.ru", "123");
+
 
     }
 }
